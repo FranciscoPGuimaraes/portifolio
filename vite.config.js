7084dbafc113ac7a9a68918590c0d652574894e1
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-03-07 12:47:51 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-});
+export default {
+  base: "./", // Caminho relativo para garantir que os assets sejam encontrados
+};
