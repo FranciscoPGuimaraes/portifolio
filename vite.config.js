90a6ee8b5e61bff4a9cea7bb3b4eb5efbf35a40e
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-03-07 14:01:59 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [vue(), react()],
+    plugins: [react()],
     resolve: {
         alias: {
             src: path.resolve(__dirname, './src')
         }
     }
 });
+
